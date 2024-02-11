c625806c425978387b27dd2c2d8c1cb98742a8ba
Arunjith Surendran <arunjithsurendran22@gmail.com>
2024-02-11 15:56:12 +0530

    mobile

--- src/components/authorization/api.jsx
+++ src/components/authorization/api.jsx
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: "https://react-food-delivery-h5oa.onrender.com/api/v1/vendor",
 });
 
+// https://react-food-delivery-h5oa.onrender.com/api/v1/vendor
+// http://localhost:3000/api/v1/user
 // Request interceptor for adding the bearer token
 api.interceptors.request.use(
   (config) => {
