d8fdc2e6e672e8497b2c0bb272298fd5ac2b94f6
Arunjith Surendran <arunjithsurendran22@gmail.com>
2024-02-14 18:42:22 +0530

    grpsh

--- src/components/authorization/api.jsx
+++ src/components/authorization/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://react-food-delivery-h5oa.onrender.com/api/v1/vendor",
+  baseURL: "http://localhost:3000/api/v1/vendor",
 });
 
 // https://react-food-delivery-h5oa.onrender.com/api/v1/vendor
